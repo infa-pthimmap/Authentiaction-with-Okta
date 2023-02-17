d41b057ce680958364e4e60960d65520270da1c6
Pavan Thimmappa <pthimmappa@informatica.com>
2023-02-17 14:57:56 +0530

    fix

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export const oktaConfig = {
 	issuer: "https://sso.informatica.com",
 	clientId: "0oa1mqropuduibN8X1d8",
+	baseUrl: "https://sso.informatica.com",
 	redirectUri: `${window.location.origin}/login/callback`,
 	scopes: ["openid", "profile", "email", "groups"],
 	pkce: true,
