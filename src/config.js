a528872ec4047c51c11cb6290e2538c6cd0baa7d
Pavan Kumar <58762864+infa-pthimmap@users.noreply.github.com>
2023-02-17 12:21:13 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const oktaConfig = {
-	issuer: `https://${process.env.REACT_APP_OKTA_BASE}/oauth2/default`,
+	issuer: `https://${process.env.REACT_APP_OKTA_BASE}`,
 	clientId: process.env.REACT_APP_OKTA_CLIENT,
 	redirectUri: `${window.location.origin}/login/callback`,
-	scopes: ["openid", "profile", "email"],
+	scopes: ["openid", "profile", "email", "groups"],
 	pkce: true,
 };
